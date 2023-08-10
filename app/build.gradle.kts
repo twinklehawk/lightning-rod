8e3d68089a8bf5346422e6c370e010582571e308
Flexo <twinklebot@plshark.net>
2023-08-10 11:00:37 +0000

    Update plugin io.spring.dependency-management to v1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.9.0"
     id("org.springframework.boot") version "3.1.2"
-    id("io.spring.dependency-management") version "1.1.2"
+    id("io.spring.dependency-management") version "1.1.3"
     jacoco
     id("io.gitlab.arturbosch.detekt") version "1.23.1"
     id("org.jmailen.kotlinter") version "3.15.0"
