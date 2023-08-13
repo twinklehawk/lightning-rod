9c5b565ab4ce7054f1eb0e6dcfa36e6d16371bde
Flexo <twinklebot@plshark.net>
2023-08-13 06:00:36 +0000

    Update plugin org.jmailen.kotlinter to v3.16.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.3"
     jacoco
     id("io.gitlab.arturbosch.detekt") version "1.23.1"
-    id("org.jmailen.kotlinter") version "3.15.0"
+    id("org.jmailen.kotlinter") version "3.16.0"
 }
 
 repositories {
