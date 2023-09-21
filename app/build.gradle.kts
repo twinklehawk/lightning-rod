4c981ba654ece120e4b70bc72e07d8fe46cb906c
Flexo <twinklebot@plshark.net>
2023-09-21 14:09:26 +0000

    Update plugin org.jetbrains.kotlin.jvm to v1.9.10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.0"
+    kotlin("jvm") version "1.9.10"
     id("org.springframework.boot") version "3.1.4"
     kotlin("plugin.spring") version "1.9.0"
     id("io.spring.dependency-management") version "1.1.3"
